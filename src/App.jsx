8470613fdb4622860371804ea08db6608dd09da1
Madhu <kmadhucse2026@gmail.com>
2026-09-07 21:21:22 +0530

    Fix GitHub Pages React Router basename

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { TaskProvider } from "./context/TaskContext";
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      basename={import.meta.env.BASE_URL.replace(/\/$/, "")}
+    >
       <TaskProvider>
         <AppRoutes />
       </TaskProvider>
     </BrowserRouter>
   );
 }
+
+export default App;
